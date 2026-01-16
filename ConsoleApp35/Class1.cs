57c17b993bc6dcfb44a33ae9c6aecdcc4aa1bedb
Dima <mityahearthstone@gmail.com>
2026-01-17 01:10:20 +0200

    updated

---
+++ ConsoleApp35/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp35
+{
+    class Account
+    {
+        private string name;
+        private double balance;
+        private double irlBalance;
+
+        public Account(string name, double balance, double irlBalance)
+        {
+            this.name = name;
+            this.balance = balance;
+            this.irlBalance = irlBalance;
+        }
+
+        public double Withdrawal(double input_withdrawal)
+        {
+            if (input_withdrawal >= 0 && input_withdrawal <= balance)
+            {
+                balance = balance - input_withdrawal;
+                irlBalance = irlBalance + input_withdrawal;
+                return balance;
+            }
+            else
+            {
+                Console.WriteLine("Erorr!");
+                return balance;
+            }
+
+        }
+
+        public double Deposit(double input_deposit)
+        {
+            if (input_deposit > 0 && input_deposit <= irlBalance)
+            {
+                balance = balance + input_deposit;
+                irlBalance = irlBalance - input_deposit;
+                return balance;
+            }
+            else
+            {
+                Console.WriteLine("Error");
+                return balance;
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Account name: {name} account balance: {balance}");
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+    }
+
+
+}
--- ConsoleApp35/Program.cs
+++ ConsoleApp35/Program.cs
@@ -1,66 +1,20 @@
-﻿
-Account heikkisAccount = new Account("Heikki's account", 100.00, 95.00);
-Account heikkisSwissAccount = new Account("Heikki's account in Switzerland", 1000000.00, 2511.00);
+﻿using ConsoleApp35;
+using System;
 
-
-heikkisAccount.Withdrawal(20);
-Console.WriteLine("The balance of Heikki's account is now: " + heikkisAccount.GetBalance());
-heikkisSwissAccount.Deposit(200);
-Console.WriteLine("The balance of Heikki's other account is now: " + heikkisSwissAccount.GetBalance());
-
-class Account
+namespace ConsoleApp35
 {
-    private string name;
-    private double balance;
-    private double irlBalance;
-
-    public Account(string name, double balance, double irlBalance)
-        {
-            this.name = name;
-            this.balance = balance;
-            this.irlBalance = irlBalance;        
-        }
-
-    public double Withdrawal(double input_withdrawal)
+    class Program
     {
-        if (input_withdrawal >= 0 && input_withdrawal <= balance)
+        static void Main()
         {
-            balance = balance - input_withdrawal;
-            irlBalance = irlBalance + input_withdrawal;
-            return balance;
-        }
-        else
-        {
-            Console.WriteLine("Erorr!");
-            return balance;
-        }
+            Account heikkisAccount = new Account("Heikki's account", 100.00, 95.00);
+            Account heikkisSwissAccount = new Account("Heikki's account in Switzerland", 1000000.00, 2511.00);
 
-    }
+            heikkisAccount.Withdrawal(20);
+            Console.WriteLine("The balance of Heikki's account is now: " + heikkisAccount.GetBalance());
 
-    public double Deposit(double input_deposit)
-    {
-        if (input_deposit > 0 && input_deposit <= irlBalance)
-        {
-            balance = balance + input_deposit;
-            irlBalance = irlBalance - input_deposit;
-            return balance;
-        }
-        else
-        {
-            Console.WriteLine("Error");
-            return balance;
+            heikkisSwissAccount.Deposit(200);
+            Console.WriteLine("The balance of Heikki's other account is now: " + heikkisSwissAccount.GetBalance());
         }
     }
-
-    public void Print()
-    {
-        Console.WriteLine($"Account name: {name} account balance: {balance}");
-    }
-    
-    public double GetBalance()
-    {
-        return balance;
-    }
-
 }
-
